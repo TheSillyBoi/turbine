4a190dab292890add747bb66ce126bc4b1ace49c
ErrorCode0 <me@errorcodezero.dev>
2025-06-10 23:00:58 -0700

    Adding DUMP, LDA, PUSH, POP

--- src/util.c
+++ src/util.c
@@ -1,9 +1,9 @@
 #include "util.h"
 #include <stdint.h>
 
-void u16_split(uint16_t value, uint8_t *low_byte, uint8_t *high_byte) {
-  *low_byte = (uint8_t)(value & 0xFF);
-  *high_byte = (uint8_t)(value >> 8);
+void u16_split(uint16_t value, uint8_t *left, uint8_t *right) {
+  *left = (uint8_t)(value & 0xFF);
+  *right = (uint8_t)(value >> 8);
 }
 
 uint16_t u16_combine(uint8_t left, uint8_t right) {
--- src/vm.c
+++ src/vm.c
@@ -12,11 +12,11 @@ VirtualMachine init_vm() {
   memset(memory, 0x0, MEMORY_SIZE);
 
   VirtualMachine vm = {0};
-  vm.base_pointer.word = RAM_END;
-  vm.stack_pointer.word = RAM_END;
-  vm.general_register.word = 0x0;
+  vm.base_pointer = RAM_END;
+  vm.stack_pointer = RAM_END;
+  vm.general_register = 0x0;
   memset(vm.memory, 0, MEMORY_SIZE);
-  vm.instruction_pointer.word = 0x0;
+  vm.instruction_pointer = 0x0;
   vm.flags_register = 0x0;
 
   return vm;
@@ -70,19 +70,106 @@ void step(VirtualMachine *vm) {
   case DUMP: {
     uint8_t flag = deref_indirect(vm->instruction_pointer++, vm, 0);
     uint8_t mem_addr = deref_indirect(vm->instruction_pointer++, vm, 0);
+    switch (flag) {
+    case 0x0: {
+      vm->memory[mem_addr] = vm->general_register;
+      break;
+    }
+    case 0x1: {
+      uint8_t *left = 0;
+      uint8_t *right = 0;
+      u16_split(vm->general_register, left, right);
+      vm->memory[mem_addr] = *left;
+      vm->memory[mem_addr + 1] = *right;
+      break;
+    }
+    case 0x2: {
+      uint8_t *left = 0;
+      uint8_t *right = 0;
+      u16_split(vm->stack_pointer, left, right);
+      vm->memory[mem_addr] = *left;
+      vm->memory[mem_addr + 1] = *right;
+      break;
+    }
+    case 0x3: {
+      uint8_t *left = 0;
+      uint8_t *right = 0;
+      u16_split(vm->base_pointer, left, right);
+      vm->memory[mem_addr] = *left;
+      vm->memory[mem_addr + 1] = *right;
+      break;
+    }
+    }
     break;
   }
   case LDA: {
     uint8_t flag = deref_indirect(vm->instruction_pointer++, vm, 0);
     uint8_t mem_addr = deref_indirect(vm->instruction_pointer++, vm, 0);
+    switch (flag) {
+    case 0x0: {
+      vm->general_register = mem_addr;
+      break;
+    }
+    case 0x1: {
+      vm->stack_pointer = mem_addr;
+      break;
+    }
+    case 0x2: {
+      vm->base_pointer = mem_addr;
+      break;
+    }
+    }
     break;
   }
+  // least significant first, most significant last for push
+  // reverse for pop
   case PUSH: {
     uint8_t flag = deref_indirect(vm->instruction_pointer++, vm, 0);
     uint8_t mem_addr = deref_indirect(vm->instruction_pointer++, vm, 0);
+    uint8_t *left = 0;
+    uint8_t *right = 0;
+    u16_split(vm->general_register, left, right);
+    switch (flag) {
+    case 0x0: {
+      vm->memory[vm->stack_pointer--] = *left;
+      break;
+    }
+    case 0x1: {
+      vm->memory[vm->stack_pointer--] = *right;
+      break;
+    }
+    case 0x2: {
+      vm->memory[vm->stack_pointer--] = *left;
+      vm->memory[vm->stack_pointer--] = *right;
+      break;
+    }
+    }
     break;
   }
   case POP: {
+    uint8_t flag = deref_indirect(vm->instruction_pointer++, vm, 0);
+    uint8_t mem_addr = deref_indirect(vm->instruction_pointer++, vm, 0);
+    switch (flag) {
+    case 0x0: {
+      vm->general_register = vm->memory[vm->stack_pointer++];
+      break;
+    }
+    case 0x1: {
+      vm->general_register = u16_combine(vm->memory[vm->stack_pointer++],
+                                         vm->memory[vm->stack_pointer++]);
+      break;
+    }
+    case 0x2: {
+      vm->stack_pointer = u16_combine(vm->memory[vm->stack_pointer++],
+                                      vm->memory[vm->stack_pointer++]);
+      break;
+    }
+    case 0x3: {
+      vm->base_pointer = u16_combine(vm->memory[vm->stack_pointer++],
+                                     vm->memory[vm->stack_pointer++]);
+      break;
+    }
+    }
     break;
   }
   case ADC: {
